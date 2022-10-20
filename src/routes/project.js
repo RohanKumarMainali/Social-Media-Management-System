910173af3f2823aea66e0e6c1c2343145491156d
Samishan11 <samishanthapa0@gmail.com>
2022-10-20 13:22:31 +0545

    signuop

--- src/routes/project.js
+++ src/routes/project.js
@@ -1,15 +1,13 @@
 
 const router = require('express').Router();
-const postProject = require('../controller/postProject');
+const postProject= require('../controller/postProject');
 const getProject = require('../controller/getProjects');
 const deleteProject = require('../controller/deleteProject');
 const updateProject = require('../controller/updateProject');
-const signUp = require('../controller/admin/signUp');
 
-router.post('/signUp', signUp);
-router.post('/postProject', postProject);
-router.get('/getProject', getProject);
-router.put('/updateProject/:id', updateProject);
-router.delete('/deleteProject/:id', deleteProject);
+router.post('/postProject',postProject);
+router.get('/getProject',getProject);
+router.put('/updateProject/:id',updateProject);
+router.delete('/deleteProject/:id',deleteProject);
 
 module.exports = router;
