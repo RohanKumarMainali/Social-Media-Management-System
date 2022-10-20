336acf302fc83f7b8ffd45816d659ca07bdfe9fb
RohanKumarMainali <rohanmainali39@gmail.com>
2022-10-20 12:52:41 +0545

    login added

---
+++ src/middleware/loginAuth.js
@@ -0,0 +1,67 @@
+const jwt = require('jsonwebtoken');
+// const { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } = require("../configs/index.config"); 
+
+const ACCESS_TOKEN_KEY = "lsakdjfnsldfkjsavjs546546f6s"
+const REFRESH_TOKEN_KEY ="fsadf4654sd6f54sd1f32a1sdf65"
+
+//verify jwt token
+const VerifyJWT = async(req, res, next) => {
+  //token validation
+  if (req.header('authorization') === undefined || req.header('authorization').length <= 9) {
+    return res.status(401).send({
+      message:"Token is empty !!"
+    });
+  } 
+  var accessToken = req.header('authorization')
+
+  //remove the bearer text from token
+  accessToken = accessToken.substr(7, accessToken.length);
+  
+  
+  try {
+    const res = await jwt.verify(accessToken, ACCESS_TOKEN_KEY)
+    next()
+  } catch (err) {
+    return res.status(401).send({
+      message:"Session timeout."
+    })
+  }
+}
+
+//generate jwt token
+const GenerateJWT = (uid) => {
+  const accessToken = jwt.sign({ id: uid }, ACCESS_TOKEN_KEY, {
+    expiresIn: "24h"
+  })
+  const refreshToken = jwt.sign({ id: uid }, REFRESH_TOKEN_KEY)
+  
+  return {
+    accessToken: accessToken,
+    refreshToken:refreshToken
+  }
+}
+
+
+//regenerate the access token using refresh token
+const regenerateAccessToken = (req, res, next) => {
+  // fetch the refresh token
+  var refreshToken = req.header("authorization")
+  refreshToken = refreshToken.substr(14, refreshToken.length);
+  
+  //verify refresh token
+  try {
+    const response = jwt.verify(refreshToken, REFRESH_TOKEN_KEY);
+    
+    //regenerate the access token    
+    req.body.uid = response.id
+    
+    //shift the process
+    next()
+  } catch (error) {
+    res.status(401).send({
+      message:"Refresh token cannot verified."
+    })
+  }
+}
+
+module.exports = { VerifyJWT, GenerateJWT, regenerateAccessToken }
