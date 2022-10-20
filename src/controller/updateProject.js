6a6078a031d9530b6cea2e40d36a3e9fa04306ab
Samishan11 <samishanthapa0@gmail.com>
2022-10-20 12:46:03 +0545

    update

--- src/controller/updateProject.js
+++ src/controller/updateProject.js
@@ -6,34 +6,35 @@ const {
 
 const updateProject = async (req, res) => {
 
-    const {
-        customerName,
-        email,
-        contact,
-        address,
-        packageType,
-        billingCycle,
-        startDate,
-        endDate,
-        contract,
-        logo,
-    } = req?.body;
 
-    const id = req.params.id
+    const id = req.params.id;
 
+    const file = req?.files?.logo
     try {
+        const data = await cloudinary.uploader.upload(file.tempFilePath, {
+            folder: 'logo',
+        }, function (err, docs) {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(docs)
+            }
+        })
 
         const result = new projectModel.findByIdAndUpdate(id, {
-            customerName: customerName.toUpperCase(),
-            email: email.toUpperCase(),
-            contact: contact.toUpperCase(),
-            address: address.toUpperCase(),
-            packageType: packageType.toUpperCase(),
-            billingCycle: billingCycle.toUpperCase(),
-            startDate: startDate.toUpperCase(),
-            endDate: endDate.toUpperCase(),
-            contract: contract.toUpperCase(),
-            logo: logo.toUpperCase(),
+            customerName: req?.body?.customerName.toUpperCase(),
+            email: req?.body?.email.toUpperCase(),
+            contact: req?.body?.contact.toUpperCase(),
+            address: req?.body?.address.toUpperCase(),
+            packageType: req?.body?.packageType.toUpperCase(),
+            billingCycle: req?.body?.billingCycle.toUpperCase(),
+            startDate: req?.body?.startDate.toUpperCase(),
+            endDate: req?.body?.endDate.toUpperCase(),
+            contract: req?.body?.contract.toUpperCase(),
+            logo: {
+                public_id: data.public_id,
+                url: data.secure_url
+            },
         })
         return res.status(StatusCodes.OK).send({
             message: "Project has been updated sucessfully",
