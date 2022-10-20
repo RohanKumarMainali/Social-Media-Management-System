06f3941badafae992007b3f984b48cf0f3d50ea5
RohanKumarMainali <rohanmainali39@gmail.com>
2022-10-20 13:22:54 +0545

    test

--- src/routes/admin.js
+++
@@ -1,8 +0,0 @@
-
-const router = require('express').Router();
-const login =  require('../controller/admin/login');
-
-router.post('/login',login);
-
-
-module.exports = router;
--- src/routes/project.js
+++ src/routes/project.js
@@ -5,6 +5,9 @@ const getProject = require('../controller/getProjects');
 const deleteProject = require('../controller/deleteProject');
 const updateProject = require('../controller/updateProject');
 const signUp = require('../controller/admin/signUp');
+const login =  require('../controller/admin/login');
+
+router.post('/login',login);
 
 router.post('/signUp', signUp);
 router.post('/postProject', postProject);
