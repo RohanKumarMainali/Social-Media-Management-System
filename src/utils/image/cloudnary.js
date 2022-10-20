65807f0a0fcf003ae205dd7953ef40ce201a2885
Samishan11 <samishanthapa0@gmail.com>
2022-10-20 12:22:51 +0545

    cloudnary setup

---
+++ src/utils/image/cloudnary.js
@@ -0,0 +1,11 @@
+var cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: 'dm1p2tmnk',
+    api_key: '886767343818362',
+    api_secret: 'pali3xWFHHxYG6ulRJspYDYasTQ'
+});
+
+
+
+module.exports = cloudinary;
